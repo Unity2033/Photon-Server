982a6ddd1558a2b48763a9f8a63eb17c9b1a23ff
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-02-15 21:46:12 +0900

    Photon Login Setting

--- Photon Server Game/Assets/Script/PhotonSetting.cs
+++ Photon Server Game/Assets/Script/PhotonSetting.cs
@@ -1,4 +1,5 @@
 using Photon.Pun;
+using PlayFab;
 using PlayFab.ClientModels;
 using System.Collections;
 using System.Collections.Generic;
@@ -17,5 +18,67 @@ public class PhotonSetting : MonoBehaviourPunCallbacks
         PhotonNetwork.GameVersion = "1.0f";
 
         PhotonNetwork.LoadLevel("Photon Lobby");
+    }
+
+    public void LoginFailure(PlayFabError error)
+    {
+        NotificationManager.NotificationWindow
+        (
+            "Login Failed",
+            "There are currently no acounts registered on the server. " +
+            "\n\n Please enter your ID and password correctly"
+        );
+    }
+
+    public void SignUpSuccess(RegisterPlayFabUserResult result)
+    {
+        NotificationManager.NotificationWindow
+        (
+           "MemberShip Successful",
+           "Congratulations on becoming a member. " +
+           "\n\n Your email account has been registered on the game server"
+        );
+    }
+
+    public void SignUpFailure(PlayFabError error)
+    {
+        NotificationManager.NotificationWindow
+        (
+           "Failed to Sign Up",
+           "Membership registration failed due to a current server error. " +
+           "\n\n Please try to register as a member again"
+        );
+    }
+
+    public void SignUp()
+    {
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = email.text,
+            Password = password.text
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser
+        (
+            request,
+            SignUpSuccess,
+            SignUpFailure
+        );
+    }
+
+    public void Login()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = email.text,
+            Password = password.text
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress
+        (
+            request,
+            LoginSuccess,
+            LoginFailure
+        );
     }
 }
