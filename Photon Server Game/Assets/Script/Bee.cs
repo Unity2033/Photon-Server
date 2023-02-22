c3499784746bcb52ef02e6e4a7c805e5a5178b4f
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-02-22 21:47:31 +0900

    PhotonNetwork Object Create

---
+++ Photon Server Game/Assets/Script/Bee.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bee : MonoBehaviour
+{
+    private Vector3 direction;
+    private GameObject target;
+    private float speed = 0.1f;
+
+    void Start()
+    {
+        target = GameObject.Find("Character(Clone)");
+
+        direction = target.transform.position - transform.position;
+        transform.LookAt(target.transform);
+    }
+
+    void Update()
+    {
+        transform.position += direction.normalized * speed * Time.deltaTime;
+    }
+}
---
+++ Photon Server Game/Assets/Script/ChatManager.cs
@@ -0,0 +1,36 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatManager : MonoBehaviourPunCallbacks
+{
+    public InputField input;
+    public Transform chatContent;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            if (input.text.Length == 0) return;
+
+            string chat = PhotonNetwork.NickName + " : " + input.text;
+
+            photonView.RPC("Chatting", RpcTarget.All, chat);
+        }
+    }
+
+    [PunRPC]
+    void Chatting(string msg)
+    {
+        GameObject chat = Instantiate(Resources.Load<GameObject>("String"));
+        chat.GetComponent<Text>().text = msg;
+
+        chat.transform.SetParent(chatContent);
+
+        input.ActivateInputField();
+
+        input.text = null;
+    }
+}
